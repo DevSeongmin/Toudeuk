1728fa98fc1f3236f558a0daf8fa42ac6a94de2a
Chung <souffle1903@gmail.com>
2024-10-22 17:46:30 +0900

    [FIX]
    Former-commit-id: f19dd4aa7f39f533111eb873519d9487f8a88ff7

--- server/src/main/java/com/toudeuk/server/core/response/ErrorResponse.java
+++ server/src/main/java/com/toudeuk/server/core/response/ErrorResponse.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f59ca8163ada565f99b56361ccd53e28ccf7d9b33dd13bee9642268e458eda1d
-size 1855
+oid sha256:6a4058340c9bb27ca8d479742b74f5e63394bae3e3c6aae56b643455cdee501e
+size 1930
