7cd8f2919bf3fc8f21cd58e3b722006da3771f87
박보람 <142669102+gittidev@users.noreply.github.com>
2024-10-22 16:01:47 +0900

    [feat] 버튼 생성함
    Former-commit-id: 64357e9a82941d8f0d3c06c466fd6c17f3f1dd38

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:44af420466d9c7b8d5007ecc32b4953587b269567fc7f2a1190151962b02d724
-size 1549
+oid sha256:8666d01c7df8f9222347963cc46118ee030e0ae871abb2735e20ab0c4bd8ce4e
+size 2170
