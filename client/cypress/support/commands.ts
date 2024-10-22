8be4ea8f7ee6dca7b55bf86a00ed5a87401cf58e
박보람 <142669102+gittidev@users.noreply.github.com>
2024-10-22 17:59:33 +0900

    [FEAT] cypress 설정중
    Former-commit-id: 2848b3685067048497c38164bb96762e914c0eff

---
+++ client/cypress/support/commands.ts
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0e4227990e3c62ba02b79dfb72f8cd9686c04e993e54be98d9a5c5b79ae78d74
+size 1314
---
+++ client/cypress/support/e2e.ts
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3bea1b7a288a6be689573992a8d9b68bd0ecc718ee2b9211249d2df0f41ba2d0
+size 667
