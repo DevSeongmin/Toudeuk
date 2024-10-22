e8878f780a5ffc4eaa4d0077b1f21d92fb695ada
김연지 <yj009977@gmail.com>
2024-10-22 14:53:17 +0900

    [FEAT] 랭킹 수정중
    Former-commit-id: 026237d2b3579e6c0d7ecd0f6a7a07bdad042b3d

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:44af420466d9c7b8d5007ecc32b4953587b269567fc7f2a1190151962b02d724
-size 1549
+oid sha256:cf4758b81a1faf915003f5dea805e39485c9a8d9ef3b82ae0ce01dfbf5899db9
+size 1422
--- client/src/app/rank/components/Rank.tsx
+++ client/src/app/rank/components/Rank.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e73c83446cc3ee921e85818399bb9140a15dd4417dbd5291ae2064efaeca290f
-size 2870
+oid sha256:baf523774c28fe8bd2178bc1c417abd7dc2b26c3be135c43ae6b9fb5f8e13916
+size 3473
