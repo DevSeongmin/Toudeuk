2a4807925f4a82be064b75e5eb7122348e01768f
lij0825 <dldlswns890@gmail.com>
2024-10-23 23:49:29 +0900

    [FIX] User 유니크 제약조건 수정
    Former-commit-id: 55238a8013cb6c2250b5c6056266e59cf1fff6d6

--- server/src/main/java/com/toudeuk/server/domain/user/entity/User.java
+++ server/src/main/java/com/toudeuk/server/domain/user/entity/User.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6931a7f4d1248c397e6beef1b417e1491563fbe56b6cb15b5d846c1746396ca7
-size 3048
+oid sha256:6f52dc0c403555b0dcc8a6fe155d06feb88087ffaec044c4a23b7c75ebb73140
+size 3116
