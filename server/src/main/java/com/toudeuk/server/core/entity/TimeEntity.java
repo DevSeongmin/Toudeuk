a88bf505cb73a178e3d95dc100485196b51bb79c
SeongMin Hwang <qq221qq@naver.com>
2024-10-23 10:25:16 +0900

    설정 및 약간 수정
    Former-commit-id: e8b265ad4f9b3d2a30d5284ba2f29c0d52eec3d8

--- server/src/main/java/com/toudeuk/server/core/entity/TimeEntity.java
+++ server/src/main/java/com/toudeuk/server/core/entity/TimeEntity.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c384c906fe462ef90f2d9f5fbf18284309c2046c5e3577387eb99dc64abce7f7
+oid sha256:9bbf519f36ccf1e9c25ddda376130e3ac24dcbe1ea93465de7bc8c5f50160ab0
 size 991
--- server/src/main/java/com/toudeuk/server/domain/user/entity/User.java
+++ server/src/main/java/com/toudeuk/server/domain/user/entity/User.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d8d49ff500472c869b2508a0c9bd234f463842d664fbb55ccb02adc337c03bdc
-size 2023
+oid sha256:8b881e388c9127ecc5df313e3b58054fc0df5fa2a916ba6aa57b679f5f6af533
+size 1839
