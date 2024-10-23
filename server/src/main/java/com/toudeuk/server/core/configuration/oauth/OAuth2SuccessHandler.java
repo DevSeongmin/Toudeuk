61c3b1e397aafdbbd6b5aa34469341b02f1f7909
SeongMin Hwang <qq221qq@naver.com>
2024-10-23 23:13:30 +0900

    [FEAT] redis template
    Former-commit-id: 5bd61cd4a45e33b791fb526e5e1bbff7a4048509

--- server/src/main/java/com/toudeuk/server/core/configuration/oauth/OAuth2SuccessHandler.java
+++ server/src/main/java/com/toudeuk/server/core/configuration/oauth/OAuth2SuccessHandler.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4c5dd78e68803e825b0bbb284f60f6b72983d109ce1b265ea89b5a74f8b451b9
-size 8146
+oid sha256:14e615b6dfc74ae08335e0acb0d3718e8fede8eb1105ab234256ae7feada09f4
+size 8164
