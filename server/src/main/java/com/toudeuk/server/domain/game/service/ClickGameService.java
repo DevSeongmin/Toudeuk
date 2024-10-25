e1ffded0ffa0d0bccebd7a1c7fd2ccecb0aa49ea
이인준 <dldlswns890@naver.com>
2024-10-25 16:16:08 +0900

    [FIX] 게임 리워드 타입 추가
    Former-commit-id: 745f72ef1e86045a0062a921b6d33dc80293f5f2

--- server/src/main/java/com/toudeuk/server/domain/game/service/ClickGameService.java
+++ server/src/main/java/com/toudeuk/server/domain/game/service/ClickGameService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:27a2c104c0f825f5613a526ad4e0bafce5a279f5971e4a0eae7f6de30b6f0e54
-size 5464
+oid sha256:1040ea4a65a1f2c10d6997b67fc65eaca12bd94b8e5a89f4bfd25787880b649e
+size 5552
