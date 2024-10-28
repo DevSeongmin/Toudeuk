ba266c64722272cbaee9cb1116340c0caaff935c
김연지 <yj009977@gmail.com>
2024-10-28 11:17:45 +0900

    [FIX] 레이아웃 수정
    Former-commit-id: 8f8701155a99551cf05c8a05c3e6a933222b078b

--- client/src/app/login/components/Login.tsx
+++ client/src/app/login/components/Login.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aad27fa1e35a2f3ce684560af149c5b5b20e08e38e7946eee341605296d3d168
-size 4086
+oid sha256:332df5889c0924d1b7bbf464188dbeabc6e0adbe7dd7a8ac103c849a058098c2
+size 4088
--- client/src/app/rank/components/Rank.tsx
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:16cf3b0f5fc7237e751b9cf763f209f21623ba23a7449ba2f41c673dcbd53792
-size 8115
