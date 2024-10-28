4563a2a85ae37db4b63b04ba88f106e1fb74926f
이인준 <dldlswns890@naver.com>
2024-10-28 13:41:54 +0900

    [FIX] click 메소드 수정
    Former-commit-id: a4f85f3c0cada3a753fa43c9611b238eb52d8191

--- server/src/main/java/com/toudeuk/server/domain/game/controller/ClickGameController.java
+++ server/src/main/java/com/toudeuk/server/domain/game/controller/ClickGameController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0f1b23a0e7614493d64c5a8519a3d061115c893f9b5e362a1bd7c1c59152d992
-size 2448
+oid sha256:ce77079da17dfaf525bf74334c076c5a09cb178206886ddb20d0a0da5fdcdb91
+size 2388
