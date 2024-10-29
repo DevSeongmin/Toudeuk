edd4b4bb84c08b91c1de5b10e38dd64c9a0d27f6
sgo722@naver.com <sgo722@naver.com>
2024-10-29 13:51:32 +0900

    [fix] oauth2 properties, successHandler 수정
    Former-commit-id: db8531bb45de7bd78cec03039c0bc8620464d0bb

--- client/public/sw.js
+++ client/public/sw.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:352dad463fa98676659cc604652cdeed0d2a29788aa817442932d3cb85614f50
-size 9195
+oid sha256:2534688af49290a08f3e66a58ea3b4dee6ee476efd66e6c7cc631e2ec0a17e86
+size 2827
--- server/src/main/java/com/toudeuk/server/domain/user/handler/CustomSuccessHandler.java
+++ server/src/main/java/com/toudeuk/server/domain/user/handler/CustomSuccessHandler.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:487adf32b258baeb5d1aedbc6389202889b3de96c40ae3a22544d6a044c78b55
-size 2949
+oid sha256:ec4640e518a66c6e57472bf7d1bb826b17ce1cc4575e08375a80a4e29a0d449b
+size 2983
