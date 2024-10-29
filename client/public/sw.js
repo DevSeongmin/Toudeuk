c0f5e59fa2abf63ecb5bb6328979d6adf69045f4
이인준 <dldlswns890@naver.com>
2024-10-29 11:30:44 +0900

    [FIX] 웹 소켓 주소 변경
    Former-commit-id: 68187c8718d4eb82b2d92bb11e4b2c69376a12be

--- client/public/sw.js
+++ client/public/sw.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8fb30d9a4e49c2dc24c02158405ccb7f1e85ba11a00ed35d2244bb7ec786101a
+oid sha256:48edac23ae78a78926f101b7627269256b1db6950cbf5bdd8cf1ffbb3c7a4236
 size 9090
--- server/src/main/java/com/toudeuk/server/domain/game/service/ClickGameService.java
+++ server/src/main/java/com/toudeuk/server/domain/game/service/ClickGameService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e3706f9dc3e0ce030bf1c1757c8f87246d2e795ee225493b97f96b7d7f3d069c
-size 8059
+oid sha256:d814f661cafeb594ea9f60e237d217c7a3bb714ffaadb0e12556e1c0ecf23ca5
+size 7761
