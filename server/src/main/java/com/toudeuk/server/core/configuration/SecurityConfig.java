456afb923c07628fb5dfb8936909c3c5cd7b42d8
이인준 <dldlswns890@naver.com>
2024-10-29 17:50:09 +0900

    [FIX]
    Former-commit-id: 46f4de439d52831daa7cabeadef4a0f8e434dc71

--- server/src/main/java/com/toudeuk/server/core/configuration/SecurityConfig.java
+++ server/src/main/java/com/toudeuk/server/core/configuration/SecurityConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e378924cd35f00ef2f3f1ff713e07863952336fe54079f3539b111899c03a51b
-size 3458
+oid sha256:29b1060b62aeaaf2e7013c544ffeb6023dd1939ff35d3f91a1ca8b146d12defd
+size 3448
