bcfb09e3de86fefacb90137008f8ba3db0f25b6f
양준영 <sgo722@naver.com>
2024-10-29 14:08:28 +0900

    Update CustomSuccessHandler.java
    Former-commit-id: 4f96b964a5618da07468d6d43d53f8618472199f

--- server/src/main/java/com/toudeuk/server/domain/user/handler/CustomSuccessHandler.java
+++ server/src/main/java/com/toudeuk/server/domain/user/handler/CustomSuccessHandler.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ec4640e518a66c6e57472bf7d1bb826b17ce1cc4575e08375a80a4e29a0d449b
-size 2983
+oid sha256:6a1b9918d378b0a2bafd28dce957acaae61a20a1b939d6d71f230588f7c85a55
+size 2984
