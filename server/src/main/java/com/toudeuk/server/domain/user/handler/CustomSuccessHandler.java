531812ef075fcf615ad26805da7051499c1ed867
sgo722@naver.com <sgo722@naver.com>
2024-10-29 10:09:17 +0900

    [fix] token Header로 넘어가도록 수정
    Former-commit-id: 4d1f127dc69d50a33a0434b25e55368a84ae861c

--- server/src/main/java/com/toudeuk/server/domain/user/handler/CustomSuccessHandler.java
+++ server/src/main/java/com/toudeuk/server/domain/user/handler/CustomSuccessHandler.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6b3f449fe088810bfc94ca82bce7a792fb08f49113bce65acecd6c51aba730a8
-size 2986
+oid sha256:487adf32b258baeb5d1aedbc6389202889b3de96c40ae3a22544d6a044c78b55
+size 2949
