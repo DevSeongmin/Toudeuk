6a8e64b2bdd0dedb363e2bdd9017072e2f19aefb
이인준 <dldlswns890@naver.com>
2024-10-29 17:44:22 +0900

    [FIX]
    Former-commit-id: 0abe43411ee5ba0eb8539dc3fab0a1767f579259

--- client/src/app/toudeuk/components/Button.tsx
+++ client/src/app/toudeuk/components/Button.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:263ad0e079b3d760af4edacfdd3f5621e0fd009d3e8ba04c44a5690e7a1e3098
-size 2458
+oid sha256:288a545e69665d0bf3e3b0d629c30abe9dca5b4371ee0e7fd2cbee77066262a5
+size 2503
--- server/src/main/java/com/toudeuk/server/domain/user/handler/CustomSuccessHandler.java
+++ server/src/main/java/com/toudeuk/server/domain/user/handler/CustomSuccessHandler.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:55750e970ac1ddc6ec6c0ebc75ea2472ea9efb1ce790f83ff17147f99073a0e7
-size 2833
+oid sha256:a956364b25499692bbef52fda55fd5917cce5800fc697795860219d4d46a3a08
+size 2930
