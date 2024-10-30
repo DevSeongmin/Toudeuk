6975e036e2853fc9d258dca14e446bd40bc2497f
lij0825 <dldlswns890@gmail.com>
2024-10-31 00:19:15 +0900

    [FIX] kapay fail use client 누락 추가
    Former-commit-id: 4f8777c14a8c38345a0198b4af72352cb56b49ea

--- client/src/app/kapay/fail/page.tsx
+++ client/src/app/kapay/fail/page.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cc827ff85264499abc7f356b85d3f4e2253c51435ce07ed8eba05e0acafa9fa4
-size 1089
+oid sha256:0dd0c69f3abe7ecca6b188565217c7d002aac37a884100da668b729b1dd98427
+size 1104
