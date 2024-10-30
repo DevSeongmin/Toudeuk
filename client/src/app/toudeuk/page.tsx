28017b24d14074e23c4325d99a586c753ab3602e
yjkim9497 <yj009977@gmail.com>
2024-10-30 10:14:29 +0900

    [FIX] 로그인 수정
    Former-commit-id: 09443f36f3d25aa3e6e35c2b42189fb56d3b2c94

--- client/src/app/toudeuk/page.tsx
+++ client/src/app/toudeuk/page.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2a7dfb4c03002f8095a6a61c091cf295305947235d6f8c09f67e562e34572a49
-size 1801
+oid sha256:e23e7db39feac62c03f9fb37b7a7366896315d3219c4ba7f2822e95f87af1f12
+size 1831
