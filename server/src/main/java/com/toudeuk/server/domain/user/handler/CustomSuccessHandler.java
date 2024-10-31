acf1558e0e993115fc5cf5ae05e7c667a8669b5a
sgo722@naver.com <sgo722@naver.com>
2024-10-31 11:25:00 +0900

    [fix] Successhandler redirectURI /auth/callback 으로 변경
    Former-commit-id: f7389f0ffba3049593289674b2b6a719d3f43a6b

--- server/src/main/java/com/toudeuk/server/domain/user/handler/CustomSuccessHandler.java
+++ server/src/main/java/com/toudeuk/server/domain/user/handler/CustomSuccessHandler.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fa6cc4970107c39fa28660ab9a895cd326f0d48ea2541507f6e68c856ff3cc96
-size 2831
+oid sha256:19c8a1ec6fa02bd52d02678ff5338e3d39e83fb365b9cd19c03600c338f9f0d6
+size 2834
