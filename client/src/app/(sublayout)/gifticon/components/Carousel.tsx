faa20472d71d50626caabaf1cea28a4f0098aa36
yjkim9497 <yj009977@gmail.com>
2024-10-31 15:37:34 +0900

    [FEAT] 클릭 부분 수정
    Former-commit-id: 0e21e76db8b9b24c6f0ec0b229b3381f1fd33253

--- client/src/app/(sublayout)/gifticon/components/Carousel.tsx
+++ client/src/app/(sublayout)/gifticon/components/Carousel.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1c4277708ad8eda1a2f435f7896f7ed21382ae8deb5155b9dc32b2907ccf225c
-size 2506
+oid sha256:02d22457df2c24b6e522048a5bad9a971e85c2efe2b6189bc0d53ff8ebcdb2c1
+size 2710
---
+++ client/src/app/toudeuk/components/Button.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c7812b34a81f0ba71b450c32affcc8883f1adcc1534368ef74012800dbcb7830
+size 2796
