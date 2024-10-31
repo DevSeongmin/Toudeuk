490922a473815bb70f65145ed9d17952a664a119
sgo722@naver.com <sgo722@naver.com>
2024-10-31 10:07:51 +0900

    [config]  프로메테우스 그라파나 설정
    Former-commit-id: b4ef4a6b01b30f3bedca33864940264a39e5ee3a

--- server/src/main/java/com/toudeuk/server/domain/user/handler/CustomSuccessHandler.java
+++ server/src/main/java/com/toudeuk/server/domain/user/handler/CustomSuccessHandler.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a956364b25499692bbef52fda55fd5917cce5800fc697795860219d4d46a3a08
+oid sha256:56e9e2255817b218fbb887a0dce3e4f4217b34930ecec7496e91db4a96a6b6e6
 size 2930
