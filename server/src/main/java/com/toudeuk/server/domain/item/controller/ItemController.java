74452bfb176235649502dca9f295c9bea33ffed1
이인준 <dldlswns890@naver.com>
2024-10-31 14:48:31 +0900

    [FIX] 아이템 상세 조회 API requset 지정
    Former-commit-id: 52666d8e9781cea585f2fa3f7a18d68ed85e1605

--- server/src/main/java/com/toudeuk/server/domain/item/controller/ItemController.java
+++ server/src/main/java/com/toudeuk/server/domain/item/controller/ItemController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8fbc6a62fe3bb550079c3df70f34a44b0cd0bd7c125c56d9d79eb966827233a4
-size 2259
+oid sha256:af23f7ba78f3f48405084fb9a9d302cc5fafe70c201d127eca0e4ef0a4f10582
+size 2334
