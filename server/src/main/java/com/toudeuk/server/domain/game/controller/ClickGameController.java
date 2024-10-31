a9425e95d63baabddf03117f8eabe2bf148413e8
이인준 <dldlswns890@naver.com>
2024-10-31 17:00:25 +0900

    [FIX] 외부 이미지 URL - nextconfig 추가
    Former-commit-id: 2533e51cd5eea3b707d73ade32486de211699fac

--- server/src/main/java/com/toudeuk/server/domain/game/controller/ClickGameController.java
+++ server/src/main/java/com/toudeuk/server/domain/game/controller/ClickGameController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:427f213b1df10723213e57626ad869fc9a5237dcd5b07f658ea3e1caf7bd6b45
-size 2740
+oid sha256:e193c64bfac8ee5040423bbf7d905b5fb65bbaf9e897848d6d33df680486b3c3
+size 3166
