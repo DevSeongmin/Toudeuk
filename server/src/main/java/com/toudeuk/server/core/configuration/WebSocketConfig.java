e6a2ea8257bc66dc541cd3b13be75b81d789fca8
SeongMin Hwang <qq221qq@naver.com>
2024-10-31 16:15:46 +0900

    [FEAT] 백엔드 소켓 수정
    Former-commit-id: a65bf979e947d03b295fe4c503216b9d21dbe9b2

--- server/src/main/java/com/toudeuk/server/core/configuration/WebSocketConfig.java
+++ server/src/main/java/com/toudeuk/server/core/configuration/WebSocketConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:79b02da679981f9ce3db40ee12eb50645ca78feb8ae524b68d9166c39afb4137
-size 1225
+oid sha256:33167ce4238e0c7d1343e67c60931dd5e2714f549efbc35decedb6f0fcb86ae2
+size 1226
--- server/src/main/java/com/toudeuk/server/tmp/GameClickController.java
+++ server/src/main/java/com/toudeuk/server/tmp/GameClickController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cc08a23f03f61b23c43e32202d6906f21d1d9ab958d50558b50bcba0115d1cf7
-size 1158
+oid sha256:dd3278096a8c48012b43b695d4a671763dee518b564f33154a90a21f288cc8fc
+size 1796
