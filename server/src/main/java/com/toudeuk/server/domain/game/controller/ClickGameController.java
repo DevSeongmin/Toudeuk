f0e51b326f05a9f6773784aa7ceceae4e8a0bd2e
Chung <souffle1903@gmail.com>
2024-10-31 16:42:56 +0900

    [FIX]
    Former-commit-id: 86c20236edccc23df072e311a74aab6ca30161d8

--- server/src/main/java/com/toudeuk/server/domain/game/controller/ClickGameController.java
+++ server/src/main/java/com/toudeuk/server/domain/game/controller/ClickGameController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b9253015a5cc35c76e34c6d4055ce83c8e8fd1bf41a3fc01e5ca67c70b5da07b
-size 2903
+oid sha256:5d10a64c33e1961deac54b27e2d285f24b4193b0863b4c2cd2c3f1d3f9818cd2
+size 2863
