41a4423d4012a6d9e367311304b3290b5be55d60
박보람 <142669102+gittidev@users.noreply.github.com>
2024-11-01 08:39:00 +0900

    [FIX] sentry 설정 변경
    Former-commit-id: bee8a0d42a52e3647d855899eb6e63a0cbfa9c15

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3c29066d446fdca5c8ec76b6d3e858025c06d14dcc9c549f4dab2110fea07a80
-size 1813
+oid sha256:b502d87cffb5f472b2240890b79726ec312d44d909792c5dbf464e966d7ddcae
+size 1751
