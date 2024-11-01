5285e7d9dc6ff6f182e14f0b92cef34271493261
박보람 <142669102+gittidev@users.noreply.github.com>
2024-11-01 14:11:35 +0900

    [FIX] 빌드 위해 주석처리함
    Former-commit-id: 8ed27671efdb45f39c384a56e3b70c31530518a6

--- client/src/app/(sublayout)/mygifticon/[id]/page.tsx
+++ client/src/app/(sublayout)/mygifticon/[id]/page.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fb6c6405f3fbda85f6a595e2d36361d33cf670b341fe5e2f904d1e546a270511
-size 1350
+oid sha256:ee8a27e7920d8be8736d4f7a040e070617e2427c57edd4372f9bd711f15ab0e2
+size 1505
