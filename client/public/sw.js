8c644e221b36f1f4299dff184bd37a642706b69f
Chung <souffle1903@gmail.com>
2024-11-04 16:27:06 +0900

    [FEAT] 아이템 컬럼 추가 및 상세 페이지
    Former-commit-id: d1a00b9fc2ea18aa53a7086936589493a2c2388f

--- client/public/sw.js
+++ client/public/sw.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4ddded35517db2239af6497efce85cdd94567094aa6688127cd16553cc074865
-size 10400
+oid sha256:2534688af49290a08f3e66a58ea3b4dee6ee476efd66e6c7cc631e2ec0a17e86
+size 2827
