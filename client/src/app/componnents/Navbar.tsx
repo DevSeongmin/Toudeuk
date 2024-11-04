501f52d12bf2bd17cdc19b3def37bdd91d92ef63
박보람 <142669102+gittidev@users.noreply.github.com>
2024-11-04 19:00:26 +0900

    [FIX] navbar 경로 수정
    Former-commit-id: 062f0f1a174e7b5d197fdf4671c476095b9f3d01

--- client/src/app/componnents/Navbar.tsx
+++ client/src/app/componnents/Navbar.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:968e5e7670cf2e30ba9f8ff1d4615d944f092095552a7d73a692409ad38b60fa
-size 5582
+oid sha256:fe16837e9402d3a0df5442ef369cceb01e4e830411dc277254e689d2809a8618
+size 5608
