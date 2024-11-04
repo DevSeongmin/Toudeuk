8c2ce2c688777ab839068fd26162537672b22f7c
이인준 <dldlswns890@naver.com>
2024-11-04 14:03:20 +0900

    [FIX] 프론트 의존성 추가
    Former-commit-id: 3c4de0e8560975e007b4c308b970d9cc3c7ada27

--- client/src/app/toudeuk/components/GameButton.tsx
+++ client/src/app/toudeuk/components/GameButton.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fd01b7432b7f401dae9a4c9188b3d706912ab9bac70eb58f18631e172d247d8d
-size 4435
+oid sha256:199c8ac29811cdfde26330d53311ac4095f8d0770ebf959462612486b030529b
+size 4537
