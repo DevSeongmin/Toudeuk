c879cb30ef1629fed49b5c8670ae47e8dcc6c95a
yjkim9497 <yj009977@gmail.com>
2024-11-04 13:51:25 +0900

    [FIX] 메인 게임 수정
    Former-commit-id: a9b389cdf1b771c1301548b9d178c747205b7da9

--- client/src/app/toudeuk/components/GameButton.tsx
+++ client/src/app/toudeuk/components/GameButton.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b89b4480f3adf9ee9f61dbfc835192a759082c8fce39387a68841bc0e8e9b86b
-size 3528
+oid sha256:dd882c6ca7c0ad0067317ba97d31e6a4d90099852450a2aebe24f2e987400d50
+size 3843
