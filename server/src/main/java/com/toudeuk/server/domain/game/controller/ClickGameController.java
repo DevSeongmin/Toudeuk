fbce2226f7eab8b774f4f19b195fc35d5ff4ae46
sgo722@naver.com <sgo722@naver.com>
2024-11-04 12:04:52 +0900

    [fix] tmp에 존재하던 socketController 삭제, socketController과 ClickGameController 분리
    Former-commit-id: b96a08ab21083dd7b07e5e9ba577b5eb1834c7c4

--- server/src/main/java/com/toudeuk/server/domain/game/controller/ClickGameController.java
+++ server/src/main/java/com/toudeuk/server/domain/game/controller/ClickGameController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:405f87ee3b7d0faf716674664e19ea1efaed44877f76794c0bf9307b80d76d55
-size 4262
+oid sha256:8f248a3f20fa69c6d07ac65b855c2ab8e95e7e6d078980c2902f315ec489a830
+size 3768
---
+++ server/src/main/java/com/toudeuk/server/domain/game/controller/ClickGameSocketController.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:39f48dcfd0e3801321fa924e5767884fcbb1b7d3adeae6a22e47ad1643d093ec
+size 1218
