7b173f927f36b5a08349e3825c115b5630667d02
이인준 <dldlswns890@naver.com>
2024-11-05 10:43:58 +0900

    [FEAT] 재시작 로직 추가
    Former-commit-id: a3dc2e0ab7c4ba3ea59f2b562da5818918cb5818

--- client/src/app/toudeuk/components/GameButton.tsx
+++ client/src/app/toudeuk/components/GameButton.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b89b4480f3adf9ee9f61dbfc835192a759082c8fce39387a68841bc0e8e9b86b
-size 3528
+oid sha256:ef2b31405770832d5e02801038d5870d4df24dda80e8e2659d4f1af1ca2ad8d3
+size 3621
--- server/src/main/java/com/toudeuk/server/domain/game/service/ClickGameService.java
+++ server/src/main/java/com/toudeuk/server/domain/game/service/ClickGameService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6b3764bcc52982d3bebfaf47bf0ba270dce5192a841485e7d7a0f9a3ad2bec9f
-size 12366
+oid sha256:9b99a15ac7d6142e1323ce5c757835e8014c80e245eab587fc4d7f3b17b71769
+size 12717
