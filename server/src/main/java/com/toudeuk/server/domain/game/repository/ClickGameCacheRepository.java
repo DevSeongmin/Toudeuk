c3ade486b6881ea714ba83dac44a1bf4dd429174
Chung <souffle1903@gmail.com>
2024-11-05 13:02:30 +0900

    [FEAT] 랭킹 반환
    Former-commit-id: 8ef65ff87d998ab6c940b6f8290de86f9804bf56

--- server/src/main/java/com/toudeuk/server/domain/game/repository/ClickGameCacheRepository.java
+++ server/src/main/java/com/toudeuk/server/domain/game/repository/ClickGameCacheRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8271e5fe1321fb3c43028062f40bed57aa36842156be5d7fa0366e0c009cde2a
-size 4505
+oid sha256:cb08cbef812f162baafb71d73a5b7e4aa687e4ab3fb6b3f36531cf7b0163012f
+size 4689
--- server/src/main/java/com/toudeuk/server/domain/game/service/ClickGameService.java
+++ server/src/main/java/com/toudeuk/server/domain/game/service/ClickGameService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:784dea5048ffdc7a55011c90570e74779b0de17f2d72cfb46834481cfe95edbc
-size 15132
+oid sha256:a34b48e4ec5a804aa08bc7760eca1a79429bbc5486a38a2469f6324f59712ab2
+size 15788
