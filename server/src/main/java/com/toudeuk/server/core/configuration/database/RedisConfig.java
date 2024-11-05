b0adfc229cf5db5c5d9cea361453a57c27b4513c
Chung <souffle1903@gmail.com>
2024-11-05 10:50:08 +0900

    [FIX] 레디스 트랜잭션 설정
    Former-commit-id: 211d4610cf08af1bdd14fbb53e193a0c442bd325

--- server/src/main/java/com/toudeuk/server/core/configuration/database/RedisConfig.java
+++ server/src/main/java/com/toudeuk/server/core/configuration/database/RedisConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bee551e83123a8be1374bf3c6caa3edcdb7ad0e8c6b892761532e5ab3f08c448
-size 4320
+oid sha256:b3a60d0015dbec1e713d47548f414e5eda150edabf608cd3684de4eec86b7480
+size 4588
--- server/src/main/java/com/toudeuk/server/domain/game/repository/ClickGameCacheRepository.java
+++ server/src/main/java/com/toudeuk/server/domain/game/repository/ClickGameCacheRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:325e45f3714f7ba081e94b832944d07b6b62024a12e86f6644a7230119c1ff4d
-size 4448
+oid sha256:057dc27e69acc6e9bb2754916cb4328aa151a0a7825e3684cf6e2d44f7f1e8cb
+size 4513
