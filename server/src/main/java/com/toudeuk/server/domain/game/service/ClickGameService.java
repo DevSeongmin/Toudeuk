118eb99f61c0cee7d80d95eea64bf2acbc924477
Chung <souffle1903@gmail.com>
2024-11-05 14:03:11 +0900

    [ãFIX] 랭킹 사용자 정보 추가
    Former-commit-id: 1862e9720a3a278c1445a356480dc9a98e01d7f7

--- server/src/main/java/com/toudeuk/server/domain/game/service/ClickGameService.java
+++ server/src/main/java/com/toudeuk/server/domain/game/service/ClickGameService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a3fac9d3b7756b91f8be10bc4a2406ce58e3c541a22d18be3ae9f935b4420163
-size 15746
+oid sha256:0b449f155782d670a2d96e15e95387a607f739da1556d81e06ed1a06ead6dd44
+size 15924
