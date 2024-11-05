b33d415f3bf79b006c5464db22b0792bdc904ec2
이인준 <dldlswns890@naver.com>
2024-11-05 16:12:02 +0900

    [FIX] MAX_CLICK -> 1000으로 변경
    Former-commit-id: 75be86f7fa3a5849c2c8f0ce87e8fbc55f29816d

--- server/src/main/java/com/toudeuk/server/domain/game/repository/ClickGameCacheRepository.java
+++ server/src/main/java/com/toudeuk/server/domain/game/repository/ClickGameCacheRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0474687d25ed67840277f993fbd7ebcf8f426915b1b4ff4b6e07bdcce3028829
-size 4668
+oid sha256:37a715bb79714328a378ae5cadb1d769036dc8098c50740fa8fca4f939acd228
+size 4671
