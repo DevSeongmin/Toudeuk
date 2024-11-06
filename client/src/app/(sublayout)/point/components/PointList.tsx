2513664410cb54381d79615da3a8b3dd084fdc74
yjkim9497 <yj009977@gmail.com>
2024-11-06 10:10:39 +0900

    [FIX] 포인트 내역 페이지 수정
    Former-commit-id: 5f955b3a0ff056818218efc5922473481a33e103

--- client/src/app/(sublayout)/point/components/PointList.tsx
+++ client/src/app/(sublayout)/point/components/PointList.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7b62a4b0244ba7d2b908047b96b0d24f5ff79dc78443c17ec89b4cd79799774b
-size 4279
+oid sha256:ad61ecd51fff9750ae21246b3f922b285c7b893997c8048655692bf1ffa2dd9d
+size 4395
