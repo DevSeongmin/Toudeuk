9e78ef4d13ec17342171b9c0d889f4639e776e43
이인준 <dldlswns890@naver.com>
2024-11-07 11:37:07 +0900

    [FEAT] redis에 있는 유저 캐쉬 rdb로 덮어쓰는 API 작성
    Former-commit-id: 02e671ee2047e0fa9bc54d658a53464c66e9fc89

--- server/src/main/java/com/toudeuk/server/domain/game/controller/ClickGameSocketController.java
+++ server/src/main/java/com/toudeuk/server/domain/game/controller/ClickGameSocketController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f97efa2d36fee2cf86be437a88dbf98d01079ff984dc9910cc728b49129046a8
-size 1873
+oid sha256:4b2cf5747c6ddd5879d110aa541c3f59c2ad15d50641a5164c133f9067189c2f
+size 1570
