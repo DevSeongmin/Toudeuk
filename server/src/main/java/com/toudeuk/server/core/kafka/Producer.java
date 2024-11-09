d93b5b011a65f425ef61da9f19b3d7269eae84a1
SeongMin Hwang <qq221qq@naver.com>
2024-11-09 13:24:20 +0900

    [FEAT] 컨슈머 프로그램 작성
    Former-commit-id: 917f979e294cda463965e2a3f5512cdcce008833

--- server/src/main/java/com/toudeuk/server/core/kafka/Producer.java
+++ server/src/main/java/com/toudeuk/server/core/kafka/Producer.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bc4628852c4df617a497c20a82dd8a6a77cfff4545ec03ef4c7b4eb01fa3177e
-size 1441
+oid sha256:b20d34d032e2ead87b7ec593d55cb01ce04e2b030c6190f1ca3b55a35d2f2912
+size 1378
