d54fae87cdcf2d7481ec6cac5c7a8c0092807b23
sgo722@naver.com <sgo722@naver.com>
2024-11-09 15:04:14 +0900

    [FIX] 소켓 통신 -> Http 요청으로 변경
    Former-commit-id: 673e1d08df987b4b43e49cd37d594e1b6c731103

--- server/src/main/java/com/toudeuk/server/domain/game/controller/ClickGameController.java
+++ server/src/main/java/com/toudeuk/server/domain/game/controller/ClickGameController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d260488064c2f9cdb505cc61910e2ef669dc395b0ef6f6c7d9ab69f510459438
-size 4156
+oid sha256:7240b1e9b5bcfdba781914463feed872f258970cdc3ed4f9c4423dab7827e64e
+size 4201
--- server/src/main/java/com/toudeuk/server/domain/game/service/ClickGameService.java
+++ server/src/main/java/com/toudeuk/server/domain/game/service/ClickGameService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c36353e43fe663de014b8a75ea810c9d4ad0b8ecf84809b8fb4947a7536e552a
-size 14521
+oid sha256:4955a710e3a61292b89d5d4a86c7c7b28177db73a2e60e1786bf4f80e9b6a01e
+size 14636
