0550ea69d17b12acead8b2b50b5d6053e82eced3
SeongMin Hwang <qq221qq@naver.com>
2024-11-09 13:23:19 +0900

    [FEAT] 컨슈머 프로그램 작성
    Former-commit-id: 26b181222187c3eb09c4056205a7265700685cef

--- consumer/src/main/java/com/toudeuk/Consumer.java
+++ consumer/src/main/java/com/toudeuk/Consumer.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d6bc950dd556fb2a0f63e8321e9686edd8956e332649ed20d20bca2c44a372cf
+oid sha256:88b58b5ece59ace1a16e1f62c98b3ecd3c358e8183571e5e8005bd654fe38c0e
 size 2267
