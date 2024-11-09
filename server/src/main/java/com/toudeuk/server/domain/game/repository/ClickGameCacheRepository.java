24ff80cfa791a2a504e9f2258afdf61ecb733ae2
sgo722@naver.com <sgo722@naver.com>
2024-11-09 23:27:41 +0900

    [TEST] 레디스 클릭 카운트 키 값 변경 ::  키 값 userId = 랭킹 조회 시 레디스 조회 10회 --> 키 값 username = 레디스 조회 X + 클릭 시 redis 조회 3회
    Former-commit-id: f356c4deffe9d037b63b109b519c2028147a949d

--- server/src/main/java/com/toudeuk/server/domain/game/repository/ClickGameCacheRepository.java
+++ server/src/main/java/com/toudeuk/server/domain/game/repository/ClickGameCacheRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2c1f7979764540efbf80fc457e209d06d9b7b54466ef971cf2a2d6d5b72b3447
-size 6553
+oid sha256:3f36ddb8734efd3f84de3590406f3ccf2cd54f71d3614556d66bd8704f92f134
+size 6077
