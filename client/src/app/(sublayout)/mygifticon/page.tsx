d146c56c79fdad604ac92becb3616a1a2e9de2d8
박보람 <142669102+gittidev@users.noreply.github.com>
2024-11-10 23:27:51 +0900

    [FIX] 유저 기프티콘 hook 일부 분리
    Former-commit-id: 6d451524f234fba43bde85cb2ae194df3c7d2833

--- client/src/app/(sublayout)/mygifticon/page.tsx
+++ client/src/app/(sublayout)/mygifticon/page.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b5c158d75754144176c3cc5ce2de770a7dadd18086284bb23c97c6ac1141f821
-size 7466
+oid sha256:85d5bae4a4db30b41a2f2ad4a732de9c8298a251eeb9bf60a8f4890bcae22dc9
+size 7024
