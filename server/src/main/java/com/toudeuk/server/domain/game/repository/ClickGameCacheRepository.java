7dd5ac14c40c47a6ce9901db9715b2562a24760f
sgo722@naver.com <sgo722@naver.com>
2024-11-11 15:39:11 +0900

    [TEST] 랭킹 조회 시 mysql에서 nickname꺼내서 반환하도룩 수정
    Former-commit-id: 8e84a07a0430ef6a16bd65823712abaf1dbcf90f

--- server/src/main/java/com/toudeuk/server/domain/game/repository/ClickGameCacheRepository.java
+++ server/src/main/java/com/toudeuk/server/domain/game/repository/ClickGameCacheRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c3f4d72e7463782a570356ff6380b224a20449a2837329ed9feffaf8840abc31
-size 6439
+oid sha256:5b307ea42599eb4286dae1298e3b49afa9c398178dd51ab4ef0cd917a7ea7077
+size 6669
