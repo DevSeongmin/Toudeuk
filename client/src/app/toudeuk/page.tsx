7b15087b5cb0653e308e376bdeb11f1924c863d7
yjkim9497 <yj009977@gmail.com>
2024-11-11 13:41:01 +0900

    [FIX] 쿨타임 타입에러 수정
    Former-commit-id: d80ea480849624b3ba10f567c74a780af7ff5dde

--- client/src/app/toudeuk/page.tsx
+++ client/src/app/toudeuk/page.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:02dfe2ac7e3151a169163c9ff48b3116d82860cd0b5ce5527d4a6fd5baefd45c
-size 3887
+oid sha256:8557f576d8559665f89868be062f8c8ba0c7dce595476b81c81462983f8014f4
+size 3947
