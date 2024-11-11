0a39de420d1a71d0dfde97435b080b59a9101fa1
SeongMin Hwang <qq221qq@naver.com>
2024-11-11 14:36:54 +0900

    [FEAT] 캐시로그 저장
    Former-commit-id: 07ec8986b6338510e0e9321ffaa1f3a39fc30824

--- server/src/main/java/com/toudeuk/server/domain/game/service/ClickGameService.java
+++ server/src/main/java/com/toudeuk/server/domain/game/service/ClickGameService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:eda69ed30dc75d863c2e4d884a175f986a1f3168a3a921737d20e0712fde65bd
-size 15272
+oid sha256:7dab949c7125aede84a45882b8d326a6c2eb867fa8548097348d9c00c425f5cf
+size 16550
