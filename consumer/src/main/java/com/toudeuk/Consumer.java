265059cef2ce932d5111338078ebd42f0e65e9f0
SeongMin Hwang <qq221qq@naver.com>
2024-11-12 10:54:03 +0900

    [FIX] 컨슈머 수정
    Former-commit-id: 83c54c2f4af902e25f3bce448660dcac4c223f79

--- consumer/src/main/java/com/toudeuk/Consumer.java
+++ consumer/src/main/java/com/toudeuk/Consumer.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8c3d2aa6a071a28ab5a500baf3f3353e45131b93f86b11df345fc4ea4f087b76
-size 2737
+oid sha256:dba552f022662f1f4202a98c790d72cf82b9c5e172fd5f9de6bb92d08aa41850
+size 2789
