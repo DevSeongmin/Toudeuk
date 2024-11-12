a6a4df93b7d196ce2d8b13f692fba119d060a589
박보람 <142669102+gittidev@users.noreply.github.com>
2024-11-12 21:57:18 +0900

    [FIX] 로그인 수정
    Former-commit-id: 68df3b17861c50b4c5be492d518ff612c441a845

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ca84a5c0a8a0ef17ca93dc222791f85356ba3f4c8b490b2a12739a99d52a41ee
-size 1020
+oid sha256:e7b632f96a055d7ecf974f7963a1c1d508ac486a8b275befc4bf9275ece830b3
+size 1386
