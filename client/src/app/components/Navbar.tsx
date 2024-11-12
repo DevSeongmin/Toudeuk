011638711ff4e62e37b7e9a3a7cc314d7e80b5d3
박보람 <142669102+gittidev@users.noreply.github.com>
2024-11-12 10:30:13 +0900

    [FIX] 네브바 패딩 조정
    Former-commit-id: 46ec1987c9a4a4d1c330eaaee4f70f49a660ca5b

--- client/src/app/components/Navbar.tsx
+++ client/src/app/components/Navbar.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5ea290bd446cc1317827f912ca0061a623269aefdafd94e1cd5bad7166a75569
-size 5908
+oid sha256:f84635c2ab6a772c4555ae1536d88ad6d2e23604197801e688881bd627d8d4e8
+size 5923
