a75c8b2035fda78dbef11152816b801d44fce391
박보람 <142669102+gittidev@users.noreply.github.com>
2024-11-12 14:25:45 +0900

    [FIX] 핀치 줌 수정
    Former-commit-id: d8d3a754ddbbc5953bcf1ba334e29431178fc417

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0c7e2c44089f7c6f553d7e9011edc053766a69be6fe14f07ea34f0dee36aca66
-size 2442
+oid sha256:e36164326f6261aef3d0b2cc263889288ccee4c5fca796bd062ed6186113db41
+size 2462
