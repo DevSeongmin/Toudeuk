564be354fc8cfc3f06cb87d8f922b9121063bde3
박보람 <142669102+gittidev@users.noreply.github.com>
2024-11-12 10:23:33 +0900

    [FEAT] 당첨내역
    Former-commit-id: e6020ddcd23e4b308eb94be021e1569e749af798

--- client/src/app/(sublayout)/history/components/HistoryList.tsx
+++ client/src/app/(sublayout)/history/components/HistoryList.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:13bb20278244de4c29a150bcce6a4ad85eea0612aa8810db75d754d43728a1f1
-size 3293
+oid sha256:7235ac2b497a8eb5c5cc3e2487c567c2ad8c9e618abff0c25c62a487ddfc41fd
+size 3961
