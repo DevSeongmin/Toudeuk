bc4206c4bedd08223c4ad3b976f0043a71bf2f05
이인준 <dldlswns890@naver.com>
2024-11-12 14:47:54 +0900

    [FIX] FIRST 타입으로 저장되게 수정
    Former-commit-id: 21fd5c6c2910d343bc95fd65f4ed56019ded655a

--- server/src/main/java/com/toudeuk/server/domain/game/service/ClickGameService.java
+++ server/src/main/java/com/toudeuk/server/domain/game/service/ClickGameService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3abc64782fe577e81447cd233478c58b7e7f953ba3a7fe7d39349d075a9d7a5f
-size 15632
+oid sha256:6740c488d0970f61bf7a0a0f0d82f66c460af49450c0afd4bca36a61139e6d35
+size 15630
