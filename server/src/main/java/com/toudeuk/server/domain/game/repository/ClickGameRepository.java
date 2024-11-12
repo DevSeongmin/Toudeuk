7c565900ee53fcd6d31617c127d18ad8caa48a37
sgo722@naver.com <sgo722@naver.com>
2024-11-12 14:20:34 +0900

    [FEAT] 비관락 구현
    Former-commit-id: e2f65d33becd10d4c38bc79dcfe3fc2280708725

--- server/src/main/java/com/toudeuk/server/domain/game/repository/ClickGameRepository.java
+++ server/src/main/java/com/toudeuk/server/domain/game/repository/ClickGameRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:53f447aabc572d2d235df2a91600dcb13e36a66ed895f5c5b0c3e11e3f396e99
-size 831
+oid sha256:da40c4c15f0cf277603e3667bf392cfb9be305af56e3a7da1cd3f09e412a1087
+size 1090
