0e8ca73e44dfff1ab4f01404134721393a8e4055
yjkim9497 <yj009977@gmail.com>
2024-11-13 17:34:35 +0900

    [FIX] 버튼 클릭 이벤트 수정
    Former-commit-id: 9b5d3942e5a42a4dc81e09395cd93c3bcd293c21

--- client/src/app/toudeuk/components/GameButton.tsx
+++ client/src/app/toudeuk/components/GameButton.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c9f6224b5c51609dab1b206ac00b2be10562e9a82dabd29994d083599e23d270
-size 1138
+oid sha256:0fa96b5f074df7eb12edfb6786d3282ad7a3c7f96cde4d0e4c02fcdb0da0f053
+size 1788
