28a45ee8577c51f760c49101660612855e03b09c
박보람 <142669102+gittidev@users.noreply.github.com>
2024-11-13 22:38:27 +0900

    [FIX] 게임 종료 화면 디자인
    Former-commit-id: 789592f24fefce5d70c33fc02f991d691358fbbe

--- client/src/app/components/Navbar.tsx
+++ client/src/app/components/Navbar.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f84635c2ab6a772c4555ae1536d88ad6d2e23604197801e688881bd627d8d4e8
-size 5923
+oid sha256:1f50a84741bc564d589e71c58288eabdd87a2bf50545b505b9df453523f9b0ac
+size 5924
--- client/src/app/toudeuk/components/EndGame.tsx
+++ client/src/app/toudeuk/components/EndGame.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:886aa11026cb260549dbc4f966d47f63aaa206ab6d6c7aa24e23bac7e2abf1ae
-size 7972
+oid sha256:24ffd0bb972da5254da04bb60dd61233de4360e648dcf90ee5adc3033e7fddd8
+size 9907
