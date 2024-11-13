3566c9205e70a24ce65e2b350cc4966f21f67ad1
yjkim9497 <yj009977@gmail.com>
2024-11-13 11:10:22 +0900

    [FIX] reward 수정
    Former-commit-id: 4cd901a5555a60a0f27305a2d42b1f4c43fdc835

--- server/src/main/java/com/toudeuk/server/domain/game/service/ClickGameService.java
+++ server/src/main/java/com/toudeuk/server/domain/game/service/ClickGameService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:22699cba3d7164cdd9b57d0bab1ebe829e98967b813e3e513b9bc11f1bd144cc
-size 15667
+oid sha256:52a62d8866be4c6206e37fa5d3254795a93e7bf35acfc1eb6d1b46bf84040726
+size 15669
