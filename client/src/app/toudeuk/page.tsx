59d8c3eb6a0a30c7e4dcdff9586ae5dd24dfe5b5
yjkim9497 <yj009977@gmail.com>
2024-11-13 12:01:22 +0900

    [FIX] 캐시 부족 에러 추가, 빌드 에러 수정
    Former-commit-id: 5aba8b07c0a99fce2c76bbc5026e7f1273b0f3df

--- client/src/app/toudeuk/page.tsx
+++ client/src/app/toudeuk/page.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:562193d663ca66238a0c21520e5a6dc8e64237f3a6cdd974ca3bdbe484e9c804
-size 9435
+oid sha256:55a9b541e5cbdbd94e6ace2c50283888a8701bdfa2e3673467869e417f73591f
+size 10279
