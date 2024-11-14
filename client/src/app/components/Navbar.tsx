25e3eb8ee9db8023003ccb4e315d1dc1b14eb17c
박보람 <142669102+gittidev@users.noreply.github.com>
2024-11-14 14:17:05 +0900

    [FIX] navbar pb 추가
    Former-commit-id: 819ace543b6b6c2cd2ab2cb9ddfb97e8cfec66fe

--- client/src/app/components/Navbar.tsx
+++ client/src/app/components/Navbar.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a64629b4be69d2f304c2e82680b9c80cce666e3812d2f96fb78dc2390cc3ec3e
+oid sha256:e5eac666629e8d52e2aac6572a7b88ec6759ec43debeb94cf769e2a9914e2fc2
 size 5930
