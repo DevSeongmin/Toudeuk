4186e02a301a2ea04094dd2e2e4bd1c4cd8de1e5
yjkim9497 <yj009977@gmail.com>
2024-11-14 09:26:37 +0900

    [FIX] 당첨 gif 크기 조정
    Former-commit-id: 4999fd91d63d7fdddc695e855beecfb875befeb4

--- client/src/app/toudeuk/page.tsx
+++ client/src/app/toudeuk/page.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d884d01d0f6a19592972d0009f2f21b066caac8d6fd21660a179bd6017a8e944
+oid sha256:bb7c9c2fe3fbe1448c648fa4193e85bf96be4189dd18def32c7b89a51d68695a
 size 10268
