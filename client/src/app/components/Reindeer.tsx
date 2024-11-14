e4fe6bb3667f11b91ebdfd4262efcf0ae93cc19a
박보람 <142669102+gittidev@users.noreply.github.com>
2024-11-14 18:01:08 +0900

    [FIX] 루돌프
    Former-commit-id: 2ad88dd53b587bdccae36c64a7cb8b7a3ec65343

--- client/src/app/components/Reindeer.tsx
+++ client/src/app/components/Reindeer.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:78563ac34f1798b1fc0c20cb279dcf9b4409d0cc4cef0fbdf9e4f5fb5f56f8a4
-size 1645
+oid sha256:189215d3658eb9a2d20653a519942e05d51c6146e33b9875479871defdd35d4d
+size 1960
