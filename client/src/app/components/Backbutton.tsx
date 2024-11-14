86bcbe552f862b413d6290bda2795402a5954545
박보람 <142669102+gittidev@users.noreply.github.com>
2024-11-14 13:56:39 +0900

    [FIX] 뒤로가기 수정
    Former-commit-id: 761e99c61cf9b5daef43bb7f4b7cdf2dbfdaa6b0

--- client/src/app/components/Backbutton.tsx
+++ client/src/app/components/Backbutton.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:579852f1a169c5d76d1fa6ac76206942277f6263e725cef1f32a3ae4d4ff4578
-size 750
+oid sha256:e9367762b9e2a904950b0286e21eeccc7df5cd95574cdfa405f63280df892de2
+size 805
