ca89a3c50df6d070306284a2257d6acb1de70da1
SeongMin Hwang <qq221qq@naver.com>
2024-11-15 21:15:54 +0900

    [FIX] 최근 게임 보상 수정
    Former-commit-id: eaed84129a5990d03d77b22d54907ee30814100c

--- server/src/main/java/com/toudeuk/server/domain/game/service/ClickGameService.java
+++ server/src/main/java/com/toudeuk/server/domain/game/service/ClickGameService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:16a9ddda5bc04e270fab87f9178b8cd4d2d5ee5edb04973cbcbfa5131e92097c
-size 17681
+oid sha256:59c7a3056d171436d187ce0deabd55b4026a86146bda38dcbd750007259637c5
+size 17739
