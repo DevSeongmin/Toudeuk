a4e98a6eef708d3cff087c06473a741cd70c224f
이인준 <dldlswns890@naver.com>
2024-11-15 15:53:20 +0900

    [FIX] 캐쉬 결제 레디스 업데이트 수정
    Former-commit-id: 96fa61f33895d21460ad9afa66de19c7852c055a

--- server/src/main/java/com/toudeuk/server/domain/kapay/service/KapayService.java
+++ server/src/main/java/com/toudeuk/server/domain/kapay/service/KapayService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:57254edf72f180e0b218fbc8239106fb11547515b832da4ebebf29dbbad31b33
-size 5743
+oid sha256:f42e89fe674ffa22b3856a23e4d158284c71d894ce587942dcd73206dfe5c35b
+size 5952
