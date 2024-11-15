b20590e07b07294a7d39c07743b852594e20f98f
박보람 <142669102+gittidev@users.noreply.github.com>
2024-11-15 20:44:43 +0900

    [FIX] 부적절한 css 수정 및 이미지 최적화
    Former-commit-id: 7d262b9a2c1994bde362c1f22163271f0eb6773c

--- client/src/app/(sublayout)/gifticon/[id]/components/Detail.tsx
+++ client/src/app/(sublayout)/gifticon/[id]/components/Detail.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d106e689a49ae50f1efa81b683001075b377e966619afa51314e812eebb99093
-size 3528
+oid sha256:cc6952a7400c572c72f5707e4f4c0c78a7e9ef55df332389ff253430ac49e501
+size 3513
