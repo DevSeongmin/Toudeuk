9df8df2c71d13b55d3573d114eb376ea69a5f213
박보람 <142669102+gittidev@users.noreply.github.com>
2024-11-17 19:31:54 +0900

    [FIX] navbar zIndex 수정, 게임 대기 중에도 이동 가능
    Former-commit-id: 3be9a4d5d914febd6011b930392514d37b8c1f41

--- client/src/app/components/Navbar.tsx
+++ client/src/app/components/Navbar.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a0cc901f36708c6192392899a51f20033d9825dc58b95732c6ba5fd0621a9e2c
-size 5347
+oid sha256:1ae5c1f839f773d4d522ff1c16adadc25537b81e2e049fedff14c80bdcfc3c16
+size 5348
