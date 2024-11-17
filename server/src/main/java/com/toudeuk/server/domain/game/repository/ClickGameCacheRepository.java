056c7201ebef4b388c3246b8e1281a3de87b28f1
sgo722@naver.com <sgo722@naver.com>
2024-11-17 15:28:28 +0900

    [TEST] LOG 삭제
    Former-commit-id: c6bb132f6e247963f88e62fa02424716f8b3ae1f

--- server/src/main/java/com/toudeuk/server/domain/game/repository/ClickGameCacheRepository.java
+++ server/src/main/java/com/toudeuk/server/domain/game/repository/ClickGameCacheRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a5e996ed22607eb9508f23b63b0ae11f0348cfdec494c5768a8d7a0027cf9275
-size 8056
+oid sha256:95aa953640f97e28ee42b5ebfbdc17dc21e2ec0c82941f15de80593c6c8e85ca
+size 8058
