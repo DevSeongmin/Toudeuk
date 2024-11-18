67e519687f17952bbf3dc925dd5f5d72792b4efc
SeongMin Hwang <qq221qq@naver.com>
2024-11-18 12:03:37 +0900

    [Front kakao url]
    Former-commit-id: 1da35570cf19b139fd443d4d5281e2315cad7b5f

--- client/src/app/components/Login.tsx
+++ client/src/app/components/Login.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9fe56392cca47e72689194e8a5960f9b5eb5540244191b697945738161bba16c
-size 528
+oid sha256:37c60d57001cabb733c901cbdca90aa7db1a73def792cc6fb8d18358ddedc4d8
+size 643
