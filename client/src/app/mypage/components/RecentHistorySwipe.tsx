499de1f29115d94c6824954a7cae670ab55cfa22
Chung <souffle1903@gmail.com>
2024-11-18 17:30:53 +0900

    [FIX] 히스토리 스와이프 진행중 게임은 안보이게 수정
    Former-commit-id: ca1379d79b65518fe84113e2a3f0ca039e85a91d

--- client/src/app/mypage/components/RecentHistorySwipe.tsx
+++ client/src/app/mypage/components/RecentHistorySwipe.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:be8246f96a9f90b10e6e441d33bf48f298a348a050fcb1f0f0ee0452be3a5a33
-size 5160
+oid sha256:52c97487634c50e1901e70a9e4d8a7a2855887f9a64535037d646df441fc517b
+size 5161
