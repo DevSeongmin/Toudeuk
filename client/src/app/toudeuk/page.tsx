88dd6d2216b2a5dda0e99f2424b878521c85e66f
SeongMin Hwang <qq221qq@naver.com>
2024-11-18 15:13:27 +0900

    [BUG FIX]
    Former-commit-id: 8b2a3edab22b12dd7b0a9a4e55270b186a5a5ad2

--- client/src/app/toudeuk/page.tsx
+++ client/src/app/toudeuk/page.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e1b1ac0593b0ead7f1c0e523677762624ffec17414335845f97f2dd2777dd336
-size 10263
+oid sha256:ce45c571f53109d1e2706ce6227cf140f63d161294abb62cc9ed17282b6737fd
+size 10268
--- server/src/main/java/com/toudeuk/server/domain/game/repository/ClickGameCacheRepository.java
+++ server/src/main/java/com/toudeuk/server/domain/game/repository/ClickGameCacheRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:95aa953640f97e28ee42b5ebfbdc17dc21e2ec0c82941f15de80593c6c8e85ca
-size 8058
+oid sha256:cf737974e6756b4477075af2da25273a17c11e677a4d6dbe48f95ccd8c3f2cef
+size 8015
