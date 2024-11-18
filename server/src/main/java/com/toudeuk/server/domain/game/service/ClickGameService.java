8b80d36f21efb7fb626e61654ae5bb5a32aa0c50
SeongMin Hwang <qq221qq@naver.com>
2024-11-18 10:17:25 +0900

    [BUG FIX]
    Former-commit-id: de816ee8beedcde8d99bf3806de5c0450414cc7c

--- server/src/main/java/com/toudeuk/server/domain/game/service/ClickGameService.java
+++ server/src/main/java/com/toudeuk/server/domain/game/service/ClickGameService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ca6e932458a9ec0fc5c65ca3f58e3afad24ea198176e0d78d03182f92b9153c0
-size 17741
+oid sha256:5d642a9728379ba40e62dd10f82ff632a8beb797a5a5287069e7d8d7c8e4c9e4
+size 17740
