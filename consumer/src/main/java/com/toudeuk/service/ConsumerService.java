734e489017e6f096fb416d9210ee9ff90cb8e151
SeongMin Hwang <qq221qq@naver.com>
2024-11-18 11:29:45 +0900

    [FIX] 캐시로그 컨슈머에 추가
    Former-commit-id: f64d38f0d00bb3cd0f99f5c6892fffd9be12b9d5

--- consumer/src/main/java/com/toudeuk/service/ConsumerService.java
+++ consumer/src/main/java/com/toudeuk/service/ConsumerService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7808854ad5ef9412f771f68b7eee5a76ad24ad6f6f2c408e6de2c00cd3f69760
-size 3214
+oid sha256:7b0b7e19fe0c304feca9994defad23621556ad7b6771ceda88ff4a4b2c07d175
+size 3478
